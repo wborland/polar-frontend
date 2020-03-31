485eb5cc465092a3aec3417074980765a33db558
AdamKogut <akogut@purdue.edu>
2020-03-31 00:09:17 -0400

    WIP add individual table support

--- src/Redux/tables/index.js
+++ src/Redux/tables/index.js
@@ -1,6 +1,4 @@
-import {
-  GET_TABLE_LIST
-} from "../action_types";
+import { GET_TABLE_LIST, GET_INDIV_TABLE } from "../action_types";
 import axios from "axios";
 import { message } from "antd";
 
@@ -10,37 +8,52 @@ const saveTableList = list => ({
   list
 });
 
+const saveIndivTable = table => ({
+  type: GET_INDIV_TABLE,
+  table
+});
+
 // Initial dialog state
 const initialState = {
-  tableList: []
+  tableList: [],
+  tableInfo: {
+    tableTitle: "",
+    tableHeaders: [],
+    tableCell: []
+  }
 };
 
 // Action helpers
 export const getTableList = auth => dispatch => {
-  axios.post("/table/all", { "auth": auth })
-    .then((response) => {
+  axios
+    .post("/table/all", { auth: auth })
+    .then(response => {
       let tableList = [];
-      if(response.data) {
-        for(let key in response.data) {
+      if (response.data) {
+        for (let key in response.data) {
           tableList.push({
-            "key": response.data[key][0],
-            "tableName": response.data[key][1]
+            key: response.data[key][0],
+            tableName: response.data[key][1]
           });
         }
       }
       dispatch(saveTableList(tableList));
     })
-    .catch( (err) => {
+    .catch(err => {
       message.error("Something went wrong, please try again", 5);
     });
 };
+
+// export const getIndivTable=(auth, )
 
 const tableListReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_TABLE_LIST:
       return Object.assign({}, state, {
-        tableList: action.list,
+        tableList: action.list
       });
+    case GET_INDIV_TABLE:
+      return Object.assign({}, state, { tableInfo: action.table });
     default:
       return state;
   }
