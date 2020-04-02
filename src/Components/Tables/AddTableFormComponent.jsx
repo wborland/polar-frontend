328167a010013c09eb4b6ef4ec09e2a1bd943b6d
Kunal Sinha <kunalsinha98@gmail.com>
2020-04-02 16:17:18 -0400

    Fixed spacing issue

--- src/Components/Tables/AddTableFormComponent.jsx
+++ src/Components/Tables/AddTableFormComponent.jsx
@@ -49,7 +49,7 @@ class AddTableFormComponent extends Component {
               rules: [
                 {
                   type: "string",
-                  pattern: "^[a-zA-Z0-9() ]+$",
+                  pattern: "^[a-zA-Z0-9 ]+$",
                   message: "Please enter letters or numbers only"
                 },
                 {
